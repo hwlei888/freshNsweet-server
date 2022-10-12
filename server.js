4c75c8d9a4a955ebc33ca341c92c0dbc16ec6068
hwlei888 <leihuawen888@gmail.com>
2022-10-13 04:23:57 +1100

    Initial commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // set the CORS allow header for us on every request, for AJAX requests
 const cors = require('cors'); // check online
